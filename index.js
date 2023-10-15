f9394f59691a52f2b65e38135fed2c81f14998a8
OsmanBytyqi <osman.bytyqi@beriflapp.com>
2023-10-15 22:12:04 +0200

    add the dark theme feature

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const button = document.getElementById('menu-btn')
 const navigation = document.getElementById('menu')
-const darktheme = document.getElementById('dark-mode')
 const navbar = document.getElementById("nav-bar")
 
 function navToggle() {
@@ -11,10 +10,39 @@ function navToggle() {
 }
 button.addEventListener('click', navToggle)
 
-function darkmode () {
-    document.body.classList.toggle('darktheme')
-    navbar.body.classList.toggle('darktheme')
-}
 
+const darktheme = document.getElementById('dark-mode')
+const root = document.querySelector(':root');
+let isDarkMode = false;
 
-darktheme.addEventListener("click", darkmode)
+const lightmode = {
+  '--color-primary-20': '#000',
+  '--color-primary-10': '#ffff',
+  '--color-primary-30': '#E7E9EB',
+  '--color-primary-40': '#f1f1f1',
+  '--color-primary-50': 'c5c4c2',
+};
+
+const darkmode = {
+  '--color-primary-10': '#1d2a35',
+  '--color-primary-20': '#ddd',
+  '--color-primary-30': '#1d2a35',
+  '--color-primary-40': '#38444d',
+  '--color-primary-50': '#38444d',
+};
+
+const toggleMode = () => {
+    if (isDarkMode) {
+        setVariables(lightmode);
+    } else {
+        setVariables(darkmode);
+    }
+    isDarkMode = !isDarkMode; // Toggle the mode
+};
+
+const setVariables = vars => {
+    Object.entries(vars).forEach(v => root.style.setProperty(v[0], v[1]));
+};
+
+darktheme.addEventListener('click', toggleMode);
+
